6ec4cdc5081ff0ed2f8b003f780ec2d33058b336
Nycolas <88675719+NycFenix@users.noreply.github.com>
2024-07-15 13:16:28 -0300

    ADD: Added button conections
    also fixed bug with class call

--- QT Tests/Calculator.py
+++ QT Tests/Calculator.py
@@ -42,7 +42,7 @@ class Ui_MyCalculator(QMainWindow):
         font.setPointSize(12)
         self.Button1.setFont(font)
         self.horizontalLayout.addWidget(self.Button1)
-
+        self.Button1.clicked.connect(lambda: self.writeExpression(self.Button1))
         # Button 2
         self.Button2 = QPushButton(self.horizontalLayoutWidget)
         self.Button2.setObjectName(u"Button2")
@@ -50,6 +50,7 @@ class Ui_MyCalculator(QMainWindow):
         self.Button2.setFont(font)
 
         self.horizontalLayout.addWidget(self.Button2)
+        self.Button2.clicked.connect(lambda: self.writeExpression(self.Button2))
         # Button 3
 
         self.Button3 = QPushButton(self.horizontalLayoutWidget)
@@ -58,6 +59,7 @@ class Ui_MyCalculator(QMainWindow):
         self.Button3.setFont(font)
 
         self.horizontalLayout.addWidget(self.Button3)
+        self.Button3.clicked.connect(lambda: self.writeExpression(self.Button3))
 
         # Button 4
         self.horizontalLayoutWidget_3 = QWidget(self.centralwidget)
@@ -72,6 +74,7 @@ class Ui_MyCalculator(QMainWindow):
         self.Button4.setFont(font)
 
         self.horizontalLayout_3.addWidget(self.Button4)
+        self.Button4.clicked.connect(lambda: self.writeExpression(self.Button4))
 
         # Button 5
         self.Button5 = QPushButton(self.horizontalLayoutWidget_3)
@@ -80,7 +83,7 @@ class Ui_MyCalculator(QMainWindow):
         self.Button5.setFont(font)
 
         self.horizontalLayout_3.addWidget(self.Button5, 0, Qt.AlignHCenter)
-
+        self.Button5.clicked.connect(lambda: self.writeExpression(self.Button5))
         # Button 6
         self.Button6 = QPushButton(self.horizontalLayoutWidget_3)
         self.Button6.setObjectName(u"Button6")
@@ -88,7 +91,7 @@ class Ui_MyCalculator(QMainWindow):
         self.Button6.setFont(font)
 
         self.horizontalLayout_3.addWidget(self.Button6)
-
+        self.Button6.clicked.connect(lambda: self.writeExpression(self.Button6))
         # Button 7
         self.horizontalLayoutWidget_2 = QWidget(self.centralwidget)
         self.horizontalLayoutWidget_2.setObjectName(u"horizontalLayoutWidget_2")
@@ -102,7 +105,7 @@ class Ui_MyCalculator(QMainWindow):
         self.Button7.setFont(font)
 
         self.horizontalLayout_2.addWidget(self.Button7)
-        
+        self.Button7.clicked.connect(lambda: self.writeExpression(self.Button7))
         # Button 8
         self.Button8 = QPushButton(self.horizontalLayoutWidget_2)
         self.Button8.setObjectName(u"Button8")
@@ -110,6 +113,7 @@ class Ui_MyCalculator(QMainWindow):
         self.Button8.setFont(font)
 
         self.horizontalLayout_2.addWidget(self.Button8, 0, Qt.AlignHCenter)
+        self.Button8.clicked.connect(lambda: self.writeExpression(self.Button8))
 
         # Button 9
         self.Button9 = QPushButton(self.horizontalLayoutWidget_2)
@@ -118,6 +122,7 @@ class Ui_MyCalculator(QMainWindow):
         self.Button9.setFont(font)
 
         self.horizontalLayout_2.addWidget(self.Button9)
+        self.Button9.clicked.connect(lambda: self.writeExpression(self.Button9))
 
         # Button 0
         self.Button10 = QPushButton(self.centralwidget)
@@ -125,6 +130,9 @@ class Ui_MyCalculator(QMainWindow):
         self.Button10.setGeometry(QRect(250, 460, 75, 79))
         self.Button10.setMaximumSize(QSize(171, 101))
         self.Button10.setFont(font)
+        self.Button10.clicked.connect(lambda: self.writeExpression(self.Button10))
+
+        # Operation Buttons
         self.gridLayoutWidget = QWidget(self.centralwidget)
         self.gridLayoutWidget.setObjectName(u"gridLayoutWidget")
         self.gridLayoutWidget.setGeometry(QRect(460, 190, 211, 161))
@@ -135,39 +143,45 @@ class Ui_MyCalculator(QMainWindow):
         self.PlusButton.setObjectName(u"PlusButton")
         self.PlusButton.setMaximumSize(QSize(171, 101))
         self.PlusButton.setFont(font)
-
-        # Operation Buttons
+        
         self.gridLayout_2.addWidget(self.PlusButton, 0, 0, 1, 1)
+        self.PlusButton.clicked.connect(lambda: self.writeExpression(self.PlusButton))
+
 
         self.MinusButton = QPushButton(self.gridLayoutWidget)
         self.MinusButton.setObjectName(u"MinusButton")
         self.MinusButton.setMaximumSize(QSize(171, 101))
         self.MinusButton.setFont(font)
-
         self.gridLayout_2.addWidget(self.MinusButton, 0, 1, 1, 1)
+        self.MinusButton.clicked.connect(lambda: self.writeExpression(self.MinusButton))
 
         self.TimesButton = QPushButton(self.gridLayoutWidget)
         self.TimesButton.setObjectName(u"TimesButton")
         self.TimesButton.setMaximumSize(QSize(171, 101))
         self.TimesButton.setFont(font)
-
         self.gridLayout_2.addWidget(self.TimesButton, 1, 0, 1, 1)
+        self.TimesButton.clicked.connect(lambda: self.writeExpression(self.TimesButton))
 
         self.DivisionButton = QPushButton(self.gridLayoutWidget)
         self.DivisionButton.setObjectName(u"DivisionButton")
         self.DivisionButton.setMaximumSize(QSize(171, 101))
         self.DivisionButton.setFont(font)
-
         self.gridLayout_2.addWidget(self.DivisionButton, 1, 1, 1, 1)
+        self.DivisionButton.clicked.connect(lambda: self.writeExpression(self.DivisionButton))
 
         self.EqualButton = QPushButton(self.centralwidget)
         self.EqualButton.setObjectName(u"EqualButton")
         self.EqualButton.setGeometry(QRect(510, 410, 102, 76))
         self.EqualButton.setMaximumSize(QSize(171, 101))
         self.EqualButton.setFont(font)
+        self.EqualButton.clicked.connect(lambda: self.equalButtonClicked(self.expression))
+
         self.ClearButton = QPushButton(self.centralwidget)
         self.ClearButton.setObjectName(u"ClearButton")
         self.ClearButton.setGeometry(QRect(24, 212, 101, 51))
+        self.ClearButton.clicked.connect(lambda: self.clearExpression())
+
+
         MyCalculator.setCentralWidget(self.centralwidget)
         self.menuBar = QMenuBar(MyCalculator)
         self.menuBar.setObjectName(u"menuBar")
@@ -197,12 +211,29 @@ class Ui_MyCalculator(QMainWindow):
         self.PlusButton.setText(QCoreApplication.translate("MyCalculator", "+", None))
         self.MinusButton.setText(QCoreApplication.translate("MyCalculator", "-", None))
         self.TimesButton.setText(QCoreApplication.translate("MyCalculator", "*", None))
-        self.DivisionButton.setText(QCoreApplication.translate("MyCalculator", "\u00f7", None))
+        self.DivisionButton.setText(QCoreApplication.translate("MyCalculator", "/", None))
         self.EqualButton.setText(QCoreApplication.translate("MyCalculator", "=", None))
         self.ClearButton.setText(QCoreApplication.translate("MyCalculator", "CLEAR", None))
     # retranslateUi
 
+    def writeExpression(self, buttonPushed = QPushButton):
+        symbol = buttonPushed.text()
+        self.expression.setText(str(self.expression.text() + symbol))
 
+    def clearExpression(self):
+        self.expression.setText("")
+    def evaluateExpression(self, expression):
+        try: 
+            result = eval(expression)
+        except (ValueError, SyntaxError, ArithmeticError):
+            result = "An error has ocurred" + str(type(Exception))
+        
+        return result
+    def equalButtonClicked(self, expression = QLineEdit):
+        exp = expression.text()
+        result = str(self.evaluateExpression(exp))
+        expression.setText(result)
+        return result
 if __name__ == "__main__":
     import sys
     aplication = QApplication(sys.argv)
