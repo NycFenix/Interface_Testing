35a9d72ff3e1e1a65d2e5f3d6a6219809157777e
Nycolas <88675719+NycFenix@users.noreply.github.com>
2024-07-15 11:33:36 -0300

    Add: Expression bar

--- QT Tests/Calculator.py
+++ QT Tests/Calculator.py
@@ -7,13 +7,15 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QPalette, QPixmap, QRadialGradient, QTransform)
 from PySide6.QtWidgets import (QApplication, QGridLayout, QHBoxLayout, QMainWindow,
     QMenuBar, QPushButton, QSizePolicy, QStatusBar,
-    QWidget)
+    QWidget, QLineEdit)
 
-class Ui_MyCalculator(object):
+class Ui_MyCalculator(QMainWindow):
+
+    winW = 800
+    winH = 600
     def setupUi(self, MyCalculator):
-        if not MyCalculator.objectName():
-            MyCalculator.setObjectName(u"MyCalculator")
-        MyCalculator.resize(800, 600)
+        MyCalculator.setObjectName(u"MyCalculator")
+        MyCalculator.resize(self.winW, self.winH)
         self.centralwidget = QWidget(MyCalculator)
         self.centralwidget.setObjectName(u"centralwidget")
         self.horizontalLayoutWidget = QWidget(self.centralwidget)
@@ -22,22 +24,33 @@ class Ui_MyCalculator(object):
         self.horizontalLayout = QHBoxLayout(self.horizontalLayoutWidget)
         self.horizontalLayout.setObjectName(u"horizontalLayout")
         self.horizontalLayout.setContentsMargins(0, 0, 0, 0)
+
+
+        # Expression visor
+        font = QFont()
+        font.setFamilies([u"Book Antiqua"])
+        self.expression = QLineEdit(self.centralwidget)
+        self.expression.setFont(font)
+        font.setPointSize(16)
+        self.expression.setObjectName("expression")
+        self.expression.setFixedSize(800, 120)
+
+        # Button 1
         self.Button1 = QPushButton(self.horizontalLayoutWidget)
         self.Button1.setObjectName(u"Button1")
         self.Button1.setMaximumSize(QSize(171, 101))
-        font = QFont()
-        font.setFamilies([u"Book Antiqua"])
         font.setPointSize(12)
         self.Button1.setFont(font)
-
         self.horizontalLayout.addWidget(self.Button1)
 
+        # Button 2
         self.Button2 = QPushButton(self.horizontalLayoutWidget)
         self.Button2.setObjectName(u"Button2")
         self.Button2.setMaximumSize(QSize(171, 101))
         self.Button2.setFont(font)
 
         self.horizontalLayout.addWidget(self.Button2)
+        # Button 3
 
         self.Button3 = QPushButton(self.horizontalLayoutWidget)
         self.Button3.setObjectName(u"Button3")
@@ -46,33 +59,7 @@ class Ui_MyCalculator(object):
 
         self.horizontalLayout.addWidget(self.Button3)
 
-        self.horizontalLayoutWidget_2 = QWidget(self.centralwidget)
-        self.horizontalLayoutWidget_2.setObjectName(u"horizontalLayoutWidget_2")
-        self.horizontalLayoutWidget_2.setGeometry(QRect(170, 190, 239, 80))
-        self.horizontalLayout_2 = QHBoxLayout(self.horizontalLayoutWidget_2)
-        self.horizontalLayout_2.setObjectName(u"horizontalLayout_2")
-        self.horizontalLayout_2.setContentsMargins(0, 0, 0, 0)
-        self.Button7 = QPushButton(self.horizontalLayoutWidget_2)
-        self.Button7.setObjectName(u"Button7")
-        self.Button7.setMaximumSize(QSize(171, 101))
-        self.Button7.setFont(font)
-
-        self.horizontalLayout_2.addWidget(self.Button7)
-
-        self.Button8 = QPushButton(self.horizontalLayoutWidget_2)
-        self.Button8.setObjectName(u"Button8")
-        self.Button8.setMaximumSize(QSize(171, 101))
-        self.Button8.setFont(font)
-
-        self.horizontalLayout_2.addWidget(self.Button8, 0, Qt.AlignHCenter)
-
-        self.Button9 = QPushButton(self.horizontalLayoutWidget_2)
-        self.Button9.setObjectName(u"Button9")
-        self.Button9.setMaximumSize(QSize(171, 101))
-        self.Button9.setFont(font)
-
-        self.horizontalLayout_2.addWidget(self.Button9)
-
+        # Button 4
         self.horizontalLayoutWidget_3 = QWidget(self.centralwidget)
         self.horizontalLayoutWidget_3.setObjectName(u"horizontalLayoutWidget_3")
         self.horizontalLayoutWidget_3.setGeometry(QRect(170, 280, 239, 80))
@@ -86,6 +73,7 @@ class Ui_MyCalculator(object):
 
         self.horizontalLayout_3.addWidget(self.Button4)
 
+        # Button 5
         self.Button5 = QPushButton(self.horizontalLayoutWidget_3)
         self.Button5.setObjectName(u"Button5")
         self.Button5.setMaximumSize(QSize(171, 101))
@@ -93,6 +81,7 @@ class Ui_MyCalculator(object):
 
         self.horizontalLayout_3.addWidget(self.Button5, 0, Qt.AlignHCenter)
 
+        # Button 6
         self.Button6 = QPushButton(self.horizontalLayoutWidget_3)
         self.Button6.setObjectName(u"Button6")
         self.Button6.setMaximumSize(QSize(171, 101))
@@ -100,6 +89,37 @@ class Ui_MyCalculator(object):
 
         self.horizontalLayout_3.addWidget(self.Button6)
 
+        # Button 7
+        self.horizontalLayoutWidget_2 = QWidget(self.centralwidget)
+        self.horizontalLayoutWidget_2.setObjectName(u"horizontalLayoutWidget_2")
+        self.horizontalLayoutWidget_2.setGeometry(QRect(170, 190, 239, 80))
+        self.horizontalLayout_2 = QHBoxLayout(self.horizontalLayoutWidget_2)
+        self.horizontalLayout_2.setObjectName(u"horizontalLayout_2")
+        self.horizontalLayout_2.setContentsMargins(0, 0, 0, 0)
+        self.Button7 = QPushButton(self.horizontalLayoutWidget_2)
+        self.Button7.setObjectName(u"Button7")
+        self.Button7.setMaximumSize(QSize(171, 101))
+        self.Button7.setFont(font)
+
+        self.horizontalLayout_2.addWidget(self.Button7)
+        
+        # Button 8
+        self.Button8 = QPushButton(self.horizontalLayoutWidget_2)
+        self.Button8.setObjectName(u"Button8")
+        self.Button8.setMaximumSize(QSize(171, 101))
+        self.Button8.setFont(font)
+
+        self.horizontalLayout_2.addWidget(self.Button8, 0, Qt.AlignHCenter)
+
+        # Button 9
+        self.Button9 = QPushButton(self.horizontalLayoutWidget_2)
+        self.Button9.setObjectName(u"Button9")
+        self.Button9.setMaximumSize(QSize(171, 101))
+        self.Button9.setFont(font)
+
+        self.horizontalLayout_2.addWidget(self.Button9)
+
+        # Button 0
         self.Button10 = QPushButton(self.centralwidget)
         self.Button10.setObjectName(u"Button10")
         self.Button10.setGeometry(QRect(250, 460, 75, 79))
@@ -116,6 +136,7 @@ class Ui_MyCalculator(object):
         self.PlusButton.setMaximumSize(QSize(171, 101))
         self.PlusButton.setFont(font)
 
+        # Operation Buttons
         self.gridLayout_2.addWidget(self.PlusButton, 0, 0, 1, 1)
 
         self.MinusButton = QPushButton(self.gridLayoutWidget)
@@ -146,7 +167,7 @@ class Ui_MyCalculator(object):
         self.EqualButton.setFont(font)
         self.ClearButton = QPushButton(self.centralwidget)
         self.ClearButton.setObjectName(u"ClearButton")
-        self.ClearButton.setGeometry(QRect(24, 80, 101, 51))
+        self.ClearButton.setGeometry(QRect(24, 212, 101, 51))
         MyCalculator.setCentralWidget(self.centralwidget)
         self.menuBar = QMenuBar(MyCalculator)
         self.menuBar.setObjectName(u"menuBar")
@@ -163,20 +184,30 @@ class Ui_MyCalculator(object):
 
     def retranslateUi(self, MyCalculator):
         MyCalculator.setWindowTitle(QCoreApplication.translate("MyCalculator", u"MainWindow", None))
-        self.Button1.setText(QCoreApplication.translate("MyCalculator", u"1", None))
-        self.Button2.setText(QCoreApplication.translate("MyCalculator", u"2", None))
-        self.Button3.setText(QCoreApplication.translate("MyCalculator", u"3", None))
-        self.Button7.setText(QCoreApplication.translate("MyCalculator", u"7", None))
-        self.Button8.setText(QCoreApplication.translate("MyCalculator", u"8", None))
-        self.Button9.setText(QCoreApplication.translate("MyCalculator", u"9", None))
-        self.Button4.setText(QCoreApplication.translate("MyCalculator", u"4", None))
-        self.Button5.setText(QCoreApplication.translate("MyCalculator", u"5", None))
-        self.Button6.setText(QCoreApplication.translate("MyCalculator", u"6", None))
-        self.Button10.setText(QCoreApplication.translate("MyCalculator", u"0", None))
-        self.PlusButton.setText(QCoreApplication.translate("MyCalculator", u"+", None))
-        self.MinusButton.setText(QCoreApplication.translate("MyCalculator", u"-", None))
-        self.TimesButton.setText(QCoreApplication.translate("MyCalculator", u"*", None))
-        self.DivisionButton.setText(QCoreApplication.translate("MyCalculator", u"\u00f7", None))
-        self.EqualButton.setText(QCoreApplication.translate("MyCalculator", u"=", None))
-        self.ClearButton.setText(QCoreApplication.translate("MyCalculator", u"CLEAR", None))
+        self.Button1.setText(QCoreApplication.translate("MyCalculator", "1", None))
+        self.Button2.setText(QCoreApplication.translate("MyCalculator", "2", None))
+        self.Button3.setText(QCoreApplication.translate("MyCalculator", "3", None))
+        self.Button7.setText(QCoreApplication.translate("MyCalculator", "7", None))
+        self.Button8.setText(QCoreApplication.translate("MyCalculator", "8", None))
+        self.Button9.setText(QCoreApplication.translate("MyCalculator", "9", None))
+        self.Button4.setText(QCoreApplication.translate("MyCalculator", "4", None))
+        self.Button5.setText(QCoreApplication.translate("MyCalculator", "5", None))
+        self.Button6.setText(QCoreApplication.translate("MyCalculator", "6", None))
+        self.Button10.setText(QCoreApplication.translate("MyCalculator", "0", None))
+        self.PlusButton.setText(QCoreApplication.translate("MyCalculator", "+", None))
+        self.MinusButton.setText(QCoreApplication.translate("MyCalculator", "-", None))
+        self.TimesButton.setText(QCoreApplication.translate("MyCalculator", "*", None))
+        self.DivisionButton.setText(QCoreApplication.translate("MyCalculator", "\u00f7", None))
+        self.EqualButton.setText(QCoreApplication.translate("MyCalculator", "=", None))
+        self.ClearButton.setText(QCoreApplication.translate("MyCalculator", "CLEAR", None))
     # retranslateUi
+
+
+if __name__ == "__main__":
+    import sys
+    aplication = QApplication(sys.argv)
+    MyCalculator = QMainWindow()
+    ui = Ui_MyCalculator()
+    ui.setupUi(MyCalculator)
+    MyCalculator.show()
+    sys.exit(aplication.exec())
